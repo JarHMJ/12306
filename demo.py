c0c74b73dc64ee37d22b6ca2b4a9a8e3908add96
jarhmj <hmj849104648@gmail.com>
2018-06-15 15:43:36 +0800

    no message

--- demo.py
+++ demo.py
@@ -5,38 +5,43 @@ import json
 import ssl
 import urllib.request
 import cons
-
+import requests
 
 DATE = '2018-06-18'
-TO_STATION = '上海'
-FROM_STATION = '武汉'
+TO_STATION = 'AOH'
+FROM_STATION = 'WHN'
 
 # ssl._create_default_https_context = ssl._create_unverified_context
 
 def getlist(train_date, from_station, to_station):
-    req = urllib.request.Request(
+    # req = urllib.request.Request(
+    #     'https://kyfw.12306.cn/otn/leftTicket/queryO?leftTicketDTO.train_date=%s&leftTicketDTO.from_station=%s&leftTicketDTO.to_station=%s&purpose_codes=ADULT' % (
+    #         train_date, from_station, to_station))
+    req = requests.get(
         'https://kyfw.12306.cn/otn/leftTicket/queryO?leftTicketDTO.train_date=%s&leftTicketDTO.from_station=%s&leftTicketDTO.to_station=%s&purpose_codes=ADULT' % (
             train_date, from_station, to_station))
-    req.add_header('User-Agent',
-                   'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36')
-    html = urllib.request.urlopen(req).read()
-    html = html.decode('utf-8-sig')
-    dict = json.loads(html)
-    result = dict['data']['result']
-    for i in result:
-        if i:
-            n = i.split('|')
-            train = n[3]
-            shangwuzuo = n[32]
-            yidengzuo = n[31]
-            erdengzuo = n[30]
-            ruanwo = n[23]
-            yingwo = n[28]
-            yingzuo = n[29]
-            wuzuo = n[26]
-            print('车次:%s，商务座:%s一等座:%s，二等座:%s,软卧:%s，硬卧:%s，硬座:%s，无座:%s' % (
-                train, shangwuzuo, yidengzuo, erdengzuo, ruanwo, yingwo, yingzuo, wuzuo,))
-    return result
+    # req.add_header('User-Agent',
+    #                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36')
+    # html = urllib.request.urlopen(req).read()
+    # html = html.decode('utf-8-sig')
+    html = req.text
+    print(html)
+    # dict = json.loads(html)
+    # result = dict['data']['result']
+    # for i in result:
+    #     if i:
+    #         n = i.split('|')
+    #         train = n[3]
+    #         shangwuzuo = n[32]
+    #         yidengzuo = n[31]
+    #         erdengzuo = n[30]
+    #         ruanwo = n[23]
+    #         yingwo = n[28]
+    #         yingzuo = n[29]
+    #         wuzuo = n[26]
+    #         print('车次:%s，商务座:%s一等座:%s，二等座:%s,软卧:%s，硬卧:%s，硬座:%s，无座:%s' % (
+    #             train, shangwuzuo, yidengzuo, erdengzuo, ruanwo, yingwo, yingzuo, wuzuo,))
+    # return result
 
 
 def stations_name(name):
